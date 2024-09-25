9a048c422a88f18b7ead5a7785cb1692e948ca58
Effortless <imdejan00@gmail.com>
2024-09-25 23:37:58 +0200

    Transactions CRUD implemented

--- Controllers/TransactionsController.js
+++ Controllers/TransactionsController.js
@@ -51,67 +51,50 @@ const addTransaction = async (req, res) => {
   let serverError;
 
   for (e of orderToAdd) {
-    // Early boundary
-    if(invalidArticle || invalidQuatity || serverError)
-      return;
-    
     let { id, quantity } = e;
     // Checking the validity of arguments
     if(!id) {
-      invalidArticle = true;
-      invalidArticleStatus = 400;
-      return;
+      return res.status(400).json({
+        message: "Artikal nije dat",
+      })
     }
     if(!quantity) {
-      invalidQuatity = true;
-      return;
+      return res.status(400).json({
+        message: "Kvantitet nije dat",
+      })
     }
 
     if(!mongoose.isValidObjectId(id)) {
-      invalidArticle = true;
-      invalidArticleStatus = 404;
-      return;
+      return res.status(404).json({
+        message: "Artikal ne postoji"
+      })
     }
 
     quantity = Number(quantity);
     if(isNaN(quantity) || quantity <= 0) {
-      invalidQuatity = true;
-      return;
+      return res.status(400).json({
+        message: "Nevalidna vrednost kvantiteta",
+      })
     }
     // Check if artical exists in db
     try {
       const article = await Article.findById(id);
       if(!article) {
-        invalidArticle = true;
-        invalidArticleStatus = 404;
-        return;
+        return res.status(404).json({
+          message: "Artikal nije pronadjen",
+        })
       }
       coins += quantity * article.Price / 10;
     }
     catch(err) {
-      serverError = true;
-      return;
+      return res.status(500).json({
+        message: "Unutrasnja greska, kontaktirajte administratora",
+      })
     }
     // If all checks pass populate order
-    console.log("Pushing");
     order.push({
       Article: id,
       Quantity: quantity,
-    })
-  }
-  if(invalidArticle) {
-    return res.status(invalidArticleStatus).json({
-      message: invalidArticleStatus === 400 ? "Artikal nije dat" : "Artikal nije pronadjen",
-    })
-  }
-  if(invalidQuatity) {
-    return res.status(400).json({
-      message: "Kolicina artikala mora biti broj veci od 0",
-    })
-  }
-  if(serverError) {
-    return res.status(500).json({
-      message: "Unutrasnja greska, kontaktirajte administratora",
     })
   }
   try {
@@ -121,7 +104,7 @@ const addTransaction = async (req, res) => {
       Coins: coins,
     });
     user.Coins+= coins;
-    user.save();
+    await user.save();
     return res.status(201).json({
       transaction
     })
@@ -133,6 +116,162 @@ const addTransaction = async (req, res) => {
   }
 }
 
+const removeOrder = async (req, res) => {
+  let user;
+  const order = [];
+  let coins = 0;
+  const { orderToAdd, userId } = req.body;
+  if(!orderToAdd) {
+    return res.status(400).json({
+      message: "Artikli za tranzakciju moraju biti dati",
+    })
+  }
+  if(!userId || !mongoose.isValidObjectId(userId)) {
+    return res.status(404).json({
+      message: "Korisnik nije pronadjen"
+    })
+  }
+  // Get the user
+  try {
+    user = await User.findById(userId);
+    if(!user) {
+      return res.status(404).josn({
+        message: "Korisnik nije pronadjen",
+      })
+    }
+  }
+  catch(err) {
+    return res.status(500).json({
+      message: "Unutrasnja greska, kontaktirajte administratora",
+    })
+  }
+
+  // Check and form order
+  if(!Array.isArray(orderToAdd)) {
+    return res.status(400).json({
+      message: "Nevalidan format porudzbine",
+    })
+  }
+
+  if(orderToAdd.length === 0) {
+    return res.status(400).json({
+      message: "Porudzbine prazne",
+    })
+  }
+  for (e of orderToAdd) {
+   
+    let { id, quantity } = e;
+    // Checking the validity of arguments
+    if(!id) {
+      return res.status(400).json({
+        message: "Artikal nije dat",
+      })
+    }
+    if(!quantity) {
+      return res.status(400).json({
+        message: "Kvantitet nije dat",
+      });
+    }
+
+    if(!mongoose.isValidObjectId(id)) {
+      return res.status(404).json({
+        message: "Artikal nije pronadjen",
+      })
+    }
+
+    quantity = Number(quantity);
+    if(isNaN(quantity) || quantity <= 0) {
+      return res.status(400).json({
+        message: "Kvantitet nevalidan"
+      });
+    }
+
+    // Check if artical exists in db
+    try {
+      const article = await Article.findById(id);
+      if(!article) {
+        return res.status(404).json({
+          message: "Artikal nije pronadjen",
+        })
+      }
+      coins += quantity * article.Price;
+      if(user.Coins < coins) {
+        return res.status(400).json({
+          message: "Nedovoljno sredstava",
+        })
+      }
+    }
+    catch(err) {
+      return res.status(500).json({
+        message: "Unutrasnja greska, kontaktirajte administratora",
+      })
+    }
+
+    // If all checks pass populate order
+    order.push({
+      Article: id,
+      Quantity: quantity,
+    })
+  }
+
+  try {
+    const transaction = await Transaction.create({
+      User: userId,
+      Order: order,
+      Coins: coins * -1,
+    });
+    user.Coins-= coins;
+    await user.save();
+    return res.status(201).json({
+      transaction
+    })
+  }
+  catch(err) {
+    return res.status(500).json({
+      message: "Unutrasnja greska, kontaktirajte administratora"
+    })
+  }
+}
+
+const getAllTransactions = async (req, res) => {
+  try {
+    const transactions = await Transaction.find();
+    return res.status(200).json({
+      transactions,
+    })
+  }
+  catch(err) {
+    return res.status(500).json({
+      message: "Unutrasanj greska, kontaktirajte administratora",
+    })
+  }
+}
+
+const getUserTransactions = async (req, res) => {
+  const { id } = req.params;
+  if(!id || !mongoose.isValidObjectId(id)) {
+    return res.status(404).json({
+      message: "Korisnik nije pronadjen",
+    })
+  }
+  try {
+    const transactions = await Transaction.find({
+      User: id,
+    })
+    return res.status(200).json({
+      transactions,
+    })
+  }
+  catch(err) {
+    return res.status(500).json({
+      message: "Unutrasnja greska, kontaktirajte administratora"
+    })
+  }
+}
+
 module.exports = {
   addTransaction,
+  removeOrder,
+  getAllTransactions,
+  getUserTransactions,
 }
--- Routers/TransactionRouter.js
+++ Routers/TransactionRouter.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-const { addTransaction } = require("../Controllers/TransactionsController");
+const { addTransaction, removeOrder, getAllTransactions, getUserTransactions } = require("../Controllers/TransactionsController");
+const { workerAuth } = require("../Middleware/AuthMiddleware");
 
+// @GET /transactions
+// Gets all the transactions
+// []
+router.get("/", getAllTransactions);
+// @GET /transactions/{id}
+// Get transactions of a single user
+router.get("/:id", getUserTransactions);
 // @POST /transactions/add
 // Add a new transaction for adding to the balance
 // [WORKER]
-router.post("/add", addTransaction);
+router.post("/add", workerAuth, addTransaction);
 // @POST /transactions/remove
 // Add a new transaction for removing from the balance
 // [WORKER]
-router.post("/remove");
+router.post("/remove", workerAuth, removeOrder);
 
 module.exports = router;
